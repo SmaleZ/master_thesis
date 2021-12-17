1412e96c199fd0b25230f09d97cb22087c1e700e
zili zhang <zili.zhang@uni-ulm.de>
2021-12-17 19:19:14 +0800

    normalize the observation space using vecnormalize

--- env_wrapper.py
+++ env_wrapper.py
@@ -27,6 +27,30 @@ class DIAYN_Skill_Wrapper(Wrapper):
                                 hidden_activations="relu",
                                 output_activation='none',
                                 )
+        # TODO: from nn_builder.pytorch.CNN import CNN
+        #
+        # # PyTorch CNNs in nn_builder specifies layers in the following format:
+        # # ["conv", channels, kernel size, stride, padding]
+        # # ["maxpool", kernel size, stride, padding]
+        # # ["avgpool", kernel size, stride, padding]
+        # # ["linear", units]
+        # # where channels, kernel size, stride, padding and units must be specified as integers
+        #
+        #
+        # # This builds a CNN with 5 layers. The first layer is a convolutional layer with
+        # # 32 units, a kernel size of 3, stride of 1 and 0 padding. The next layer is a maxpool
+        # # layer with kernel size 2, stride of 2 and 0 padding. The next layer is a conv layer with
+        # # 64 units, kernel size 3, stride of 1 and 2 padding. The next layer is an average pooling layer
+        # # with kernel size 2, stride 2 and 0 padding. The final layer is a linear layer with 10 units.
+        # # The output activation is softmax, there is no dropout or batch norm and the initialiser used is xavier
+        # model = CNN(input_dim=(3, 64, 64),
+        #             layers_info=[["conv", 32, 3, 1, 0], ["maxpool", 2, 2, 0],
+        #                          ["conv", 64, 3, 1, 2], ["avgpool", 2, 2, 0],
+        #                          ["linear", 10]],
+        #             hidden_activations="relu", output_activation="softmax", dropout=0.0,
+        #             initialiser="xavier", batch_norm=True)
+        #
+        # # Note that a pytorch CNN expects input data in the form:  (batch, channels, height, width)
         self.discriminator_optimizer = Adam(self.discriminator.parameters())
 
         # skill的概率分布為均勻分布
@@ -55,6 +79,8 @@ class DIAYN_Skill_Wrapper(Wrapper):
         # print(next_state.shape)
         # 使用一種技巧計算reward
         new_reward, discriminator_outputs = self.calculate_new_reward(next_state)
+        print("next_state is:", next_state)
+        print("the shape of obs is:", next_state.shape)
         print("the pseudo reward is", new_reward)
         # discriminator 學習預測正確的skill
         self.disciminator_learn(self.skill, discriminator_outputs)
@@ -83,6 +109,8 @@ class DIAYN_Skill_Wrapper(Wrapper):
     def get_predicted_probability_of_skill(self, skill, next_state):
         # discriminator 根據next_state預測可能的skill
         predicted_probabilities_unnormalised = self.discriminator(torch.Tensor(np.array(next_state)).unsqueeze(0))
+        print("predicted_probabilities_unnormalised :", predicted_probabilities_unnormalised)
         # 正確的skill的概率
         probability_of_correct_skill = F.softmax(predicted_probabilities_unnormalised, dim=-1)[:, skill]
+        print("probability_of_correct_skill:", probability_of_correct_skill)
         return probability_of_correct_skill.item(), predicted_probabilities_unnormalised
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from env_wrapper import DIAYN_Skill_Wrapper
 import gym
 import pybulletgym
 from stable_baselines3 import SAC
+from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
 # import minerl
 import malmoenv
 from pathlib import Path
@@ -11,20 +12,23 @@ xml = Path('/home/zilizhang/DIAYN/mobchase_single_agent.xml').read_text()
 env = malmoenv.make()
 #
 env.init(xml, 9000)
-# print("before wrapper")
-#print(env.observation_space.shape)
+# # print("before wrapper")
+# #print(env.observation_space.shape)
 total_timesteps = 5000
 num_skills = 3
 
 # env = gym.make('Walker2DMuJoCoEnv-v0')
 # env = gym.make("MineRLNavigateDense-v0")
 env = DIAYN_Skill_Wrapper(env, num_skills=num_skills)
+# env = DummyVecEnv([lambda: env])
+env = DummyVecEnv([lambda: env])
+normalized_vec_env = VecNormalize(env)
 # print(env.action_space)
 # print("affter wrapper")
 # print("obs shape:")
 # print(env.observation_space.shape)
 # print(env.observation_space)
-agent = SAC("MlpPolicy", env, verbose=1, tensorboard_log="./tensorboard").learn(total_timesteps=total_timesteps)
+agent = SAC("MlpPolicy", normalized_vec_env, verbose=1, tensorboard_log="./tensorboard").learn(total_timesteps=total_timesteps)
 agent.save("sac_Walker2DMuJoCoEnv-v0")
 
 # test git
