c7923777eeea5168ed08999e80d20665a4bfbcfe
zili zhang <zili.zhang@uni-ulm.de>
2022-03-11 03:24:20 +0800

    1000000 pretraining steps for ant and change the name

--- train.py
+++ train.py
@@ -14,17 +14,17 @@ import time
 # env.init(xml, 9000)
 # # print("before wrapper")
 # #print(env.observation_space.shape)
-total_timesteps = 50000
-total_unsupervised_steps = 50000
+total_timesteps = 1000000
+total_unsupervised_steps = 1000000
 total_supervised_steps = 200000
 num_skills = 10
 
 # env = gym.make('Walker2DMuJoCoEnv-v0')
 # env = gym.make('MountainCarContinuous-v0')
 # env = gym.make('InvertedPendulum-v2')
-# env = gym.make('HalfCheetah-v2')
+env = gym.make('HalfCheetah-v2')
 # env = gym.make('Walker2d-v3')
-env = gym.make('Ant-v2')
+# env = gym.make('Ant-v2')
 # env = gym.make('Ant-v2')
 # env = gym.make('Humanoid-v2')
 # env = gym.make("MineRLNavigateDense-v0")
@@ -35,11 +35,11 @@ pretrain_env = DIAYN_Skill_Wrapper(env, num_skills=num_skills)
 
 # agent = SAC("MlpPolicy", pretrain_env, verbose=1, tensorboard_log="./tensorboard/test").learn(total_timesteps=total_timesteps)
 # agent.save("test_diayn_initialization_halfcheetah")
-for skill in [9]:
+for skill in [5,6,7,8,9]:
     continuetrain_env = DIAYN_Pretrained_Wrapper(env, skill_choosen=skill)
-    continue_train_agent = SAC.load("trainedmodel/pretrained_Ant-v2-500000.zip", env=continuetrain_env)
+    continue_train_agent = SAC.load("trainedmodel/pretrained_HalfCheetah-v2-1000000.zip", env=continuetrain_env)
     continue_train_agent.learn(total_timesteps=total_supervised_steps)
-    continutrained_path = "trainedmodel/continualtrained_{}-skill{}".format("Ant-v2", skill)
+    continutrained_path = "trainedmodel/continualtrained_{}-{}-skill{}".format("HalfCheetah-v2", total_unsupervised_steps, skill)
     continue_train_agent.save(continutrained_path)
 
 
